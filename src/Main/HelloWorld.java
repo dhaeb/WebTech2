0fff0bcc59aa14d8744fef8021d387acbe660798
dhaeb <dan.haeberlein@googlemail.com>
2017-05-03 10:36:13 +0200

    hello world hinzugefügt

--- src/Main/HelloWorld.java
+++ src/Main/HelloWorld.java
@@ -3,7 +3,7 @@ package Main;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Code hard!
 		
 		System.out.println("Hello World!") ;
 
